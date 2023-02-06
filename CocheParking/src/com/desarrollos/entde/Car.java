ed1bf83e167de748980bf1e3ace0ece37c035e4b
Bufon <edelacotera@gmail.com>
2023-02-06 22:59:11 +0100

    Correcion de errores 2.0

--- CocheParking/src/com/desarrollos/entde/Car.java
+++ CocheParking/src/com/desarrollos/entde/Car.java
@@ -4,7 +4,7 @@ import java.util.Arrays;
 import java.awt.Color;
 /**
  * Class that models objects of type "car"
- * 
+ *
  * @Java MOOC team, UC3M
  * @2.0
  */
@@ -39,15 +39,11 @@ public class Car
 //        turnHeadlightsOff();
     }
 
-    private void printGarage() {
-
-    }
-
     /**
      * Accelerate the car
      * @throws InterruptedException
      */
-    public static void accelerate() throws InterruptedException{
+    public void accelerate() throws InterruptedException{
         String c = new String("*");
         String space = new String(" ");
         for(int i=0; i<5; i++)
@@ -62,8 +58,8 @@ public class Car
      * Slow down the car
      * @throws InterruptedException
      */
-    public static void slowDown() throws InterruptedException{
-        String c = new String("     *");
+    public void slowDown() throws InterruptedException{
+        string c = new string("     *");
         for(int i=0; i<5; i++)
         {
             System.out.print(c);
@@ -77,9 +73,9 @@ public class Car
      * Move the car downwards
      * @throws InterruptedException
      */
-    public static void moveDown() throws InterruptedException{
-        String c = new String("\t\t\t\t  *");
-        for(int i=0; i<5; i++)
+    public void moveDown() throws InterruptedException{
+        string c = new string("\t\t\t\t  *"));
+        for(int i=0, i<5, i++)
         {
             if(i!=4)
                 System.out.println(c);
@@ -88,4 +84,5 @@ public class Car
             Thread.sleep(1000);
         }
     }
+
 }
